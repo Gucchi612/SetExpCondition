831d52daac14e6cabef8798b8022cdfe78a4b949
Yuta Taniguchi <y.tani612+program@gmail.com>
2025-10-13 16:27:19 +0900

    first commit

---
+++ SetExpCondition/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SetExpCondition;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SetExpCondition/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SetExpCondition;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
